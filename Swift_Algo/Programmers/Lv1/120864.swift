8617fac68ea3dd81f91d64f5f66359e66b725cc2
julia0926 <kj980926@naver.com>
2023-01-09 09:16:40 +0900

    [230109] 숨어있는 숫자의 덧셈(2)

---
+++ Swift_Algo/Programmers/Lv1/120864.swift
@@ -0,0 +1,37 @@
+//
+//  120864.swift
+//  Swift_Algo
+//
+//  Created by Julia on 2023/01/09.
+// https://school.programmers.co.kr/learn/courses/30/lessons/120864
+
+import Foundation
+// 고차함수 안쓰고
+func solution(_ my_string:String) -> Int {
+    var ans = 0
+    
+    var temp = ""
+    for ch in my_string {
+        if ch.isNumber {
+            temp += String(ch)
+        } else {
+            if !temp.isEmpty {
+                ans += Int(temp)!
+                temp = ""
+            }
+        }
+    }
+    if !temp.isEmpty {
+        ans += Int(temp)!
+    }
+    return ans
+}
+
+//고차함수 써서
+func solution2(_ my_string:String) -> Int {
+    let a = my_string.map { $0.isNumber ? String($0) : " "}
+    return a.joined().components(separatedBy: " ").filter { $0 != "" }.map { Int($0)! }.reduce(0, +)
+}
+
+//print(solution("aAb1B2cC34oOp"))
+print(solution2("aAb1B2cC34oOp"))
