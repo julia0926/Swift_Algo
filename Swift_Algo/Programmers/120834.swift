2f55d81c401df0bb0f69f6e53f693bf1ddf9fd62
julia0926 <kj980926@naver.com>
2023-01-10 09:36:19 +0900

    [230110] 외계행성의 나이

---
+++ Swift_Algo/Programmers/120834.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Swift_Algo
+//
+//  Created by Julia on 2023/01/10.
+// https://school.programmers.co.kr/learn/courses/30/lessons/120834
+
+import Foundation
+
+func solution(_ age:Int) -> String {
+    let alpha = Array("abcdefghijklmnopqrstuvwxyz")
+    var result = ""
+    for i in String(age) {
+        let idx = Int(String(i))!
+        result+=String(alpha[idx])
+    }
+    return result
+}
+
+//참고 풀이
+//func solution2(_ age:Int) -> String {
+//    let alpha = Array("abcdefghij")
+//    return String(age).map{alpha[Int(String($0))!]}.joined()
+//}
+
+print(solution(100))
